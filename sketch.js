2045ad26034a3950e3f4a4886056293e2fa6bd1a
dezert99 <bubbamart99@gmail.com>
2020-11-27 18:26:24 -0500

    reorganized project and added player roatation

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var player, bullet;
+async function setup() {
+    createCanvas(700, 600);
+    
+    player = createSprite(width/2,height/2,32,32)
+    player.addAnimation("floating",'./assets/player_32x32.png');
+
+    bullet = createSprite(width/2,200,4,4);
+    bullet.addAnimation("floating", './assets/bullet.png');
+}
+
+function draw() {
+    background(0);
+    
+    drawSprites();
+
+    if (abs(player.position.x - mouseX) > 10 && abs(player.position.y - mouseY) > 10) {
+        var x = player.position.x - mouseX
+        var y = player.position.y - mouseY
+        var angle = Math.atan2(y, x) * 180 / Math.PI
+        player.rotation = angle + 270
+      }
+}
+
+
+// //Check for chance mods
+function keyPressed() {
+  
+}
